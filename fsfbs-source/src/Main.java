ef9a513f05d70ece990f96e5eb2e504fed8ebe2d
Justin <jwyh98@gmail.com>
2018-11-10 21:16:24 +0800

    updated Main.java
    Add Steps for better direction

--- fsfbs-source/src/Main.java
+++ fsfbs-source/src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
  public static void main(String[] args){
+     //Step 1: Scan all csv including user.csv, sport_facilities.csv, booking.csv
+     // for each courts on the current day and all booking history of users automatically.
 
+     //Step 2:User Login
+
+     //Step 3: Show the recommendation algorithm and all functions available for users (or admin)
+     Scanner in = new Scanner(System.in);
  }
 }
