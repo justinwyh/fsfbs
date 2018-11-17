3538796350912fd247c7f5d57f7ee8ffa44b3eec
cklken <45100012+cklken@users.noreply.github.com>
2018-11-17 11:38:32 +0800

    Update Booking.java

--- fsfbs-source/src/Fsfbs/Booking.java
+++ fsfbs-source/src/Fsfbs/Booking.java
@@ -1,4 +1,17 @@
 package Fsfbs;
 
 public class Booking {
+	
+	private User user;
+	private String bookingID;
+	private String bookingTime;
+	private Facilities bookingFacilities;
+	
+	public addBooking(SportCentre sc, Facilities f, String t) {
+		sc.addFacilitiestoSC(facilitiesId, facilities);
+		
+	}
+	
+	
+	
 }
