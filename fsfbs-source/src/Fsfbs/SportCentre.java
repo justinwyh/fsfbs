019106358e3af51215d66fe29852f355a8d25623
Justin <jwyh98@gmail.com>
2018-11-17 10:46:19 +0800

    Update SportCentre.java

--- fsfbs-source/src/Fsfbs/SportCentre.java
+++ fsfbs-source/src/Fsfbs/SportCentre.java
@@ -1,4 +1,27 @@
 package Fsfbs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SportCentre {
+    private String scId;
+    private String scName;
+    private String scTel;
+    private String scAddress;
+    private Map<String,Facilities> facilitiesMap;
+
+
+    public SportCentre(String scId, String scName, String scTel, String scAddress){
+        this.scId = scId;
+        this.scName = scName;
+        this.scTel = scTel;
+        this.scAddress = scAddress;
+        facilitiesMap = new HashMap<>();
+    }
+
+    public void addFacilitiestoSC(String facilitiesId,Facilities facilities) {
+        facilitiesMap.put(facilitiesId,facilities);
+    }
+
+    public void removeFacilitiesFromSC 
 }
