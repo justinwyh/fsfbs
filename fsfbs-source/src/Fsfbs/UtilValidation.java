55a07d03a32035ee90a4db5ce71367fe05a84860
Justin <jwyh98@gmail.com>
2018-11-17 15:53:22 +0800

    Update UtilValidation.java

--- fsfbs-source/src/Fsfbs/UtilValidation.java
+++ fsfbs-source/src/Fsfbs/UtilValidation.java
@@ -1,5 +1,24 @@
 package Fsfbs;
 
 public class UtilValidation {
-    
+    private static UtilValidation validationInstance= new UtilValidation();
+    public static UtilValidation getValidationInstance() {return validationInstance;}
+
+    public boolean validateSportCentreById(String inputSCId){
+     Controller controller =  Controller.getInstance();
+     SportCentre sc = controller.getSportCentrebyID(inputSCId);
+     if (sc == null){
+         return false;
+     }
+     return true;
+    }
+
+    public boolean validateUserByName(String inputUserName){
+        Controller controller =  Controller.getInstance();
+        User user = controller.getUserbyID(inputUserName);
+        if (user == null){
+            return false;
+        }
+        return true;
+    }
 }
