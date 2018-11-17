cc46892808d361fd26069e55893d37b20130def4
cklken <45100012+cklken@users.noreply.github.com>
2018-11-17 10:46:39 +0800

    Update Facilities.java

--- fsfbs-source/src/Fsfbs/Facilities.java
+++ fsfbs-source/src/Fsfbs/Facilities.java
@@ -1,16 +1,18 @@
 package Fsfbs;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Facilities {
         private String facilityId; //court num can be consist of district, sport centre and facility number
         private int facilityNum;
-        private ArrayList<Booking> facilityBookingList;
+        private Map<Integer, String> timetableMap; // time, bookingID
 
         public Facilities(String facilityId) {
             this.facilityId = facilityId;
             this.facilityNum = Integer.parseInt(facilityId.substring(3));
-            facilityBookingList = new ArrayList<>();
+            timetableMap = new HashMap<>();
         }
 
     public String getFacilityId() {
@@ -21,6 +23,7 @@ public abstract class Facilities {
         return facilityNum;
     }
 
+    public void addToTimeTable()
     public void setTodayBookingList(ArrayList<Booking> bookingArrayList){
         facilityBookingList = bookingArrayList;
     }
