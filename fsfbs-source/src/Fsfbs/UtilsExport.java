3f9c3548a519f29a0e1b1001163299b1cf372fff
Jeff Ying <yingwaihei@gmail.com>
2018-11-24 16:45:32 +0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 1b91ee0960ee81b2aa1303bbc685bc34289160f1, reversing
    changes made to 14ae316897e46c77506aeeed9870091c5cf3dc0b.

--- fsfbs-source/src/Fsfbs/UtilsExport.java
+++ fsfbs-source/src/Fsfbs/UtilsExport.java
@@ -9,6 +9,7 @@ import java.io.UnsupportedEncodingException;
 
 public class UtilsExport {
 
+
 public static void printToFile(String filepath, String input[]) {
 		//
 		try {
@@ -24,6 +25,22 @@ public static void printToFile(String filepath, String input[]) {
 			e.printStackTrace();
 		}
 }
+
+
+    public static void printToFile(String filepath, String input) {
+        try {
+            PrintWriter writer;
+            writer = new PrintWriter(filepath, "UTF-8");
+            writer.print(input);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 	public static void appendToFile(String filepath, String input) throws IOException {
 		 File file = new File(filepath);
 		 FileWriter fr = new FileWriter(file, true);
