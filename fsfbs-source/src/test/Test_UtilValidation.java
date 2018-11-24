732f2e6cb508f2d341fb2ccf4967389184f94996
dondon <hohin.tang@gmail.com>
2018-11-24 14:42:05 +0800

    Load config

--- fsfbs-source/src/test/Test_UtilValidation.java
+++ fsfbs-source/src/test/Test_UtilValidation.java
@@ -1,5 +1,32 @@
 package test;
 
-public class Test_UtilValidation {
+import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import Fsfbs.UtilValidation;
+
+public class Test_UtilValidation {
+	@Test
+	public void test_Ada() throws NullPointerException, IOException {
+		UtilValidation export = new UtilValidation();
+		boolean result = export.existedAC("Ada");
+		assertEquals(true, result);
+	}
+	
+	@Test
+	public void test_Ken() throws NullPointerException, IOException {
+		UtilValidation export = new UtilValidation();
+		boolean result = export.existedAC("Ken");
+		assertEquals(true, result);
+	}
+	
+	@Test
+	public void test_Jacky() throws NullPointerException, IOException {
+		UtilValidation export = new UtilValidation();
+		boolean result = export.existedAC("Jacky");
+		assertEquals(false, result);
+	}
 }
