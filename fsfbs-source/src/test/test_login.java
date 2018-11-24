4fd51098a8caa72020b093ecc2b58fa6a7f8e902
dondon <hohin.tang@gmail.com>
2018-11-24 11:09:10 +0800

    Strange error

--- fsfbs-source/src/test/test_login.java
+++ fsfbs-source/src/test/test_login.java
@@ -1,16 +1,34 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//import static org.junit.jupiter.api.Assertions.*;
+import org.junit.*;
 import Fsfbs.*;
 
 import org.junit.jupiter.api.Test;
 
 class test_login {
 
+	class UtilTime_Stub extends UtilTime {
+		//Overwrite the class functions
+	    public String getCurrentTime(){
+	        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+	        LocalDateTime now = LocalDateTime.now();
+	        return (dtf.format(now));
+	    }
+	    
+	}
+
 	@Test
 	public void UtilTime() {
-		UtilTime time = new UtilTime();
+		boolean result;
+		UtilTime_Stub time = new UtilTime_Stub();
+		result = time.isTimeLaterThanCurrentTime("10:00:00");
+		assertEquals(true, result);
 	}
 
 }
