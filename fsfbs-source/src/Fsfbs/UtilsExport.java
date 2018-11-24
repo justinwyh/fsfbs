507dbb86de2760a6b47803ffb7cefe71b6c98b0c
polotailokyin <polo0000618@hotmail.com>
2018-11-24 13:40:20 +0800

    Added abstract class Facilities and related subclasses including badmintonCourt, TableTennisCourt and ActivityRoom.

--- fsfbs-source/src/Fsfbs/UtilsExport.java
+++ fsfbs-source/src/Fsfbs/UtilsExport.java
@@ -7,46 +7,15 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 public class UtilsExport {
-	
-<<<<<<< HEAD
-public static void printToFile(String filepath, String input[]) {		//overwrite
-	//
-	try {	
-		PrintWriter writer;
-		writer = new PrintWriter(filepath, "UTF-8");
-		for(String s: input)
-			writer.println(s);
-		if(writer!=null)
-			writer.close();
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (UnsupportedEncodingException e) {
-		e.printStackTrace();
-=======
-	public static void printToFile(String filepath, String input[]) {
-		//
-		try {	
-			PrintWriter writer;
-			writer = new PrintWriter(filepath, "UTF-8");
-			for(String s: input)
-				writer.println(s);
-			if(writer!=null)
-				writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
->>>>>>> c28bf3ee2d92973ae08cc7f5cb272c086073be7b
-	}
 
-<<<<<<< HEAD
-public static void printToFile(String filepath, String input) {		//overwrite
+public static void printToFile(String filepath, String input[]) {        //overwrite
 	//
-	try {	
+	try {
 		PrintWriter writer;
 		writer = new PrintWriter(filepath, "UTF-8");
-			writer.println(input);
+		for (String s : input)
+			writer.println(s);
+		if (writer != null)
 			writer.close();
 	} catch (FileNotFoundException e) {
 		e.printStackTrace();
@@ -60,13 +29,5 @@ public static void appendToFile(String filepath, String input) throws IOExceptio
 	 FileWriter fr = new FileWriter(file, true);
 	 fr.write(input);
 	 fr.close();
-}
-=======
-	public static void appendToFile(String filepath, String input) throws IOException {
-		 File file = new File(filepath);
-		 FileWriter fr = new FileWriter(file, true);
-		 fr.write(input);
-		 fr.close();
 	}
->>>>>>> c28bf3ee2d92973ae08cc7f5cb272c086073be7b
 }
