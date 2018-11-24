218331a088d165819e86c0adfbb89c0942bbb2a4
dondon <hohin.tang@gmail.com>
2018-11-24 12:12:32 +0800

    UtilTime Tested successfully

---
+++ fsfbs-source/src/test/Test_UtilTime.java
@@ -0,0 +1,90 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import Fsfbs.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class Test_UtilTime {
+
+	@Test
+	public void UtilTime_hour() {
+		class UtilTime_Stub extends UtilTime {
+			//Overwrite the class functions
+		    public String getCurrentTime() {
+				return "12:00:00";
+			}
+		}
+		UtilTime_Stub time = new UtilTime_Stub();
+		boolean result = time.isTimeLaterThanCurrentTime("09:00:00");
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void UtilTime_hour2() {
+		class UtilTime_Stub extends UtilTime {
+			//Overwrite the class functions
+		    public String getCurrentTime() {
+				return "12:00:00";
+			}
+		}
+		UtilTime_Stub time = new UtilTime_Stub();
+		boolean result = time.isTimeLaterThanCurrentTime("14:00:00");
+		assertEquals(true, result);
+	}
+	
+	@Test
+	public void UtilTime_min() {
+		class UtilTime_Stub extends UtilTime {
+			//Overwrite the class functions
+		    public String getCurrentTime() {
+				return "15:30:00";
+			}
+		}
+		UtilTime_Stub time = new UtilTime_Stub();
+		boolean result = time.isTimeLaterThanCurrentTime("15:29:00");
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void UtilTime_min2() {
+		class UtilTime_Stub extends UtilTime {
+			//Overwrite the class functions
+		    public String getCurrentTime() {
+				return "15:30:00";
+			}
+		}
+		UtilTime_Stub time = new UtilTime_Stub();
+		boolean result = time.isTimeLaterThanCurrentTime("15:31:00");
+		assertEquals(true, result);
+	}
+	
+	@Test
+	public void UtilTime_second() {
+		class UtilTime_Stub extends UtilTime {
+			//Overwrite the class functions
+		    public String getCurrentTime() {
+				return "20:00:30";
+			}
+		}
+		UtilTime_Stub time = new UtilTime_Stub();
+		boolean result = time.isTimeLaterThanCurrentTime("20:00:29");
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void UtilTime_second2() {
+		class UtilTime_Stub extends UtilTime {
+			//Overwrite the class functions
+		    public String getCurrentTime() {
+				return "20:00:30";
+			}
+		}
+		UtilTime_Stub time = new UtilTime_Stub();
+		boolean result = time.isTimeLaterThanCurrentTime("20:00:31");
+		assertEquals(true, result);
+	}
+}
