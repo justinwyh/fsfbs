25b8b0e33b6ab09f1cc882c09ec217b120c46b10
polotailokyin <polo0000618@hotmail.com>
2018-11-24 17:35:55 +0800

    added test utils test case

--- fsfbs-source/src/test/Test_UtilExport.java
+++ fsfbs-source/src/test/Test_UtilExport.java
@@ -7,32 +7,19 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 
 class Test_UtilExport {
-
-    /*
-YourUtil.writeFile(path, data); //whatever you have
-
-File shouldExist = new File(path);
-assertTrue(file.exists());
-// now read the file and assert the data in it is correct
-
-*/
-//        File tmpDir = new File("/var/tmp");
-//        boolean exists = tmpDir.exists();
-
 	//statement coverage
-		
 	@Test
 	public void testUtilWrite_1() {
 	    String[] s = new String[5];
 	    for (int i =0; i<5; i++){
             s[i] = "abcd";
         }
-        UtilsExport.printToFile("/Users/abcd.txt",s);
+        UtilsExport.printToFile("./Data/Membership/abc.txt",s);
 	    //logic check if file contains s
         boolean result = true;
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader("/Users/abcd.txt"));
+            reader = new BufferedReader(new FileReader("./Data/Membership/abc.txt"));
             String line = reader.readLine();
             int i=0;
             while(line != null){
@@ -40,6 +27,7 @@ assertTrue(file.exists());
                     result = false;
                     break;
                 }
+                line = reader.readLine();
             }
         } catch (FileNotFoundException e){
             result = false;
@@ -48,7 +36,6 @@ assertTrue(file.exists());
         }
         //return boolean true / f
         assertEquals(true, result);
-
 	}
 	
 
