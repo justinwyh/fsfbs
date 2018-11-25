a22011b6813ddf787231c0432cbbe21bf0c3fdc5
dondon <hohin.tang@gmail.com>
2018-11-25 12:13:51 +0800

    test user removed, changed to test_addBooking

--- fsfbs-source/src/test/Test_addBooking.java
+++ fsfbs-source/src/test/Test_addBooking.java
@@ -5,11 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class Test_User {
+class Test_addBooking {
 
 	@Test
-	public void test_searchVacancies() throws ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig, ExFacilityIdNotExist, ExFullBooking  {
-		// user calls each facility to show their vacancy
+	public void test_addBooking() throws ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig, ExFacilityIdNotExist, ExFullBooking  {
 		Controller controller = Controller.getInstance();
 		controller.importData();
 		User tester = new User("Mr A", "password", "Membership_Adult", "E1", "Facility_Badminton");
@@ -27,38 +26,27 @@ class Test_User {
 		int result = tester.getTodayBookingNum();
 		assertEquals(0, result);
 	}
-	/*
+	
 	@Test
 	public void test_addBooking3() throws ExFullBooking, ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig, ExFacilityIdNotExist {
 		Controller controller = Controller.getInstance();
 		controller.importData();
 		User tester = new User("Mr C", "password", "Membership_Adult", "E1", "Facility_Badminton");
-		tester.addBooking("E2B2", 1011);
-		tester.addBooking("E2B2", 1112);
-		tester.addBooking("E2B2", 1213);
-		tester.addBooking("E2B2", 1314);
+		tester.addBooking("E2B2", 1617);
+		tester.addBooking("E2B2", 1718);
+		tester.addBooking("E2B2", 1819);
+		tester.addBooking("E2B2", 1920);
 		int result = tester.getTodayBookingNum();
-		assertEquals(0, result);
+		assertEquals(3, result);
 	}
-	*/
-	/*
-	public void test_searchVacancies() throws ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig, ExFacilityIdNotExist  {
-		// user calls each facility to show their vacancy
+	
+	public void test_addBooking_twoPerson() throws ExFullBooking, ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig, ExFacilityIdNotExist {
 		Controller controller = Controller.getInstance();
 		controller.importData();
-		User tester = new User("Mr C", "password", "Membership_Adult", "E1", "Facility_Badminton");
-		tester.searchVacancies("E1", "badminton");
-
-	}
-	*/
-	/* Integration testing*/
-	public void test_add_search_booking() throws ExFullBooking, ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig, ExFacilityIdNotExist {
-		Controller controller = Controller.getInstance();
-		controller.importData();
-		User tester = new User("Amy", "password", "Membership_Adult", "E1", "Facility_Badminton");
+		User tester = new User("Mr A", "password", "Membership_Adult", "E1", "Facility_Badminton");
 		tester.addBooking("E1B2", 1617);
 		
-		User tester2 = new User("Bob", "password", "Membership_Adult", "E1", "Facility_Badminton");
+		User tester2 = new User("Mr B", "password", "Membership_Adult", "E1", "Facility_Badminton");
 		tester2.addBooking("E1B2", 1617);
 		int result = tester2.getTodayBookingNum();
 		assertEquals(0, result);
