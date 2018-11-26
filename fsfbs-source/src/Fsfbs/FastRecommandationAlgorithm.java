f0f616ac0dbd729aefd30233e4ce5cb698421c27
Justin <jwyh98@gmail.com>
2018-11-26 11:21:01 +0800

    Create FastRecommandationAlgorithm.java

---
+++ fsfbs-source/src/Fsfbs/FastRecommandationAlgorithm.java
@@ -0,0 +1,12 @@
+package Fsfbs;
+
+public class FastRecommandationAlgorithm {
+    private static FastRecommandationAlgorithm instance = new FastRecommandationAlgorithm();
+
+    public static FastRecommandationAlgorithm getInstance() {
+        return instance;
+    }
+    public void fastRcommandation(){
+
+    }
+}
