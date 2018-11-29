9fd72684f442de48a920001221c894e4443df720
Justin <jwyh98@gmail.com>
2018-11-30 01:10:45 +0800

    Update Test_FSFBS.java

--- fsfbs-source/src/test/Test_FSFBS.java
+++ fsfbs-source/src/test/Test_FSFBS.java
@@ -118,7 +118,7 @@ public class Test_FSFBS {
 
 		//S1B vacancy
 		@Test
-		public void test_SportFacilities5() throws ExFacilityIdNotExist, ExFacilityNameNotExist, ExSportCentreNotExist, ExTimeRangeNotCurrent, ExAllowToBookOneHourOnly, ExTimeSlotNotInOpeningHour, ExInputTimeEarlierThanCurrentTime, IOException, ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig {
+		public void test_SportFacilities51() throws ExFacilityIdNotExist, ExFacilityNameNotExist, ExSportCentreNotExist, ExTimeRangeNotCurrent, ExAllowToBookOneHourOnly, ExTimeSlotNotInOpeningHour, ExInputTimeEarlierThanCurrentTime, IOException, ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig {
 			UtilTime utilTime = UtilTime.getTimeInstance();
 			Controller controller = Controller.getInstance();
 			SportCentre temp = controller.searchSportCentre("E1");
@@ -143,94 +143,175 @@ public class Test_FSFBS {
 				assertEquals("Your inputted facility id does not exist!",e.getMessage());
 			}
 		}
-		@Test
-		public void test_searchVacancy()  {
-			setOutput();
-			User tester = new User("Mr C", "password", "Membership_Adult", "S1", "Facility_Badminton");
-			tester.searchVacancies("S1", "badminton");
-			String output = "\n" + "Sport Centre: Aberdeen Sports Centre\n" +
-					"\n" +
-					"Facility Code: S1B2\n" +
-					"1011 : Available\n" +
-					"1112 : Available\n" +
-					"1213 : Available\n" +
-					"1314 : Available\n" +
-					"1415 : Available\n" +
-					"1516 : Available\n" +
-					"1617 : Available\n" +
-					"1718 : Available\n" +
-					"1819 : Available\n" +
-					"1920 : Available\n" +
-					"2021 : Available\n" +
-					"2122 : Available\n" +
-					"2223 : Available\n" +
-					"2324 : Available\n" +
-					"\n" +
-					"Facility Code: S1B1\n" +
-					"1011 : Available\n" +
-					"1112 : Available\n" +
-					"1213 : Available\n" +
-					"1314 : Available\n" +
-					"1415 : Available\n" +
-					"1516 : Booked, Booking ID: 20181128S1B11516\n" +
-					"1617 : Available\n" +
-					"1718 : Available\n" +
-					"1819 : Available\n" +
-					"1920 : Available\n" +
-					"2021 : Available\n" +
-					"2122 : Available\n" +
-					"2223 : Available\n" +
-					"2324 : Available\n\n";
-			assertEquals(output, getOutput());
-		}
+		
+	    //S1B vacancy
+	    @Test
+	    public void test_SportFacilities5() throws ExFacilityIdNotExist, ExFacilityNameNotExist, ExSportCentreNotExist, ExTimeRangeNotCurrent, ExAllowToBookOneHourOnly, ExTimeSlotNotInOpeningHour, ExInputTimeEarlierThanCurrentTime, IOException, ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig {
+	        UtilTime utilTime = UtilTime.getTimeInstance();
+	        Controller controller = Controller.getInstance();
+	        SportCentre temp = controller.searchSportCentre("E1");
+	        ArrayList<String> list = temp.searchVacanciesInSC(1718, "badminton", utilTime);
+	        for (String txt : list)
+	            System.out.println(txt);
+	    }
 
-		//Facility Test Stub
-		@Test
-		public void test_Facility1() {
-			Facility_Badminton fb = new Facility_Badminton("E1B3");
-			assertEquals(59,fb.getPrice());
-		}
-		@Test
-		public void test_Facility2() {
-			Facility_Badminton fb = new Facility_Badminton("E1B3");
-			assertEquals("badminton court",fb.getFacilityType());
-		}@Test
-		public void test_Facility3() {
-			Facility_ActivityRoom ar = new Facility_ActivityRoom("E1A3");
-			assertEquals(99,ar.getPrice());
-		}
-		@Test
-		public void test_Facility4() {
-			Facility_ActivityRoom ar = new Facility_ActivityRoom("E1A3");
-			assertEquals("activity room",ar.getFacilityType());
-		}
-		@Test
-		public void test_Facility5() {
-			Facility_TableTennis ar = new Facility_TableTennis("E1T3");
-			assertEquals(29,ar.getPrice());
-		}
-		@Test
-		public void test_Facility6() {
-			Facility_TableTennis fb = new Facility_TableTennis("E1T3");
-			assertEquals("table tennis court",fb.getFacilityType());
-		}
+	    @Test
+	    public void test_searchVacancy() {
+	        setOutput();
+	        User tester = new User("Mr C", "password", "Membership_Adult", "E1", "Facility_Badminton");
+	        tester.searchVacancies("E1", "badminton");
+	        String output = "\n" + "Sport Centre: Java Road Sports Centre\n" +
+	                "\n" +
+	                "Facility Code: E1B1\n" +
+	                "1011 : Available\n" +
+	                "1112 : Available\n" +
+	                "1213 : Available\n" +
+	                "1314 : Available\n" +
+	                "1415 : Available\n" +
+	                "1516 : Available\n" +
+	                "1617 : Available\n" +
+	                "1718 : Available\n" +
+	                "1819 : Available\n" +
+	                "1920 : Available\n" +
+	                "2021 : Available\n" +
+	                "2122 : Available\n" +
+	                "2223 : Available\n" +
+	                "2324 : Available\n" +
+	                "\n" +
+	                "Facility Code: E1B2\n" +
+	                "1011 : Available\n" +
+	                "1112 : Available\n" +
+	                "1213 : Available\n" +
+	                "1314 : Available\n" +
+	                "1415 : Available\n" +
+	                "1516 : Available\n" +
+	                "1617 : Available\n" +
+	                "1718 : Available\n" +
+	                "1819 : Available\n" +
+	                "1920 : Available\n" +
+	                "2021 : Available\n" +
+	                "2122 : Available\n" +
+	                "2223 : Available\n" +
+	                "2324 : Available\n\n";
+	        assertEquals(output, getOutput());
+	    }
 
-		//Exception
-		@Test
-		public void test_Exception1() {
-			ExAllowToBookOneHourOnly e = new ExAllowToBookOneHourOnly();
-			assertEquals("You are allowed to book 1 hour only for each booking. Please input again.",e.getMessage());
-		}
-		@Test
-		public void test_Exception2() {
-			ExAllowToDeleteOneHourOnly e = new ExAllowToDeleteOneHourOnly();
-			assertEquals("You are allowed to book 1 hour only for each booking. Please input again.",e.getMessage());
-		}
-		@Test
-		public void test_Exception3() {
-			ExBookingHasPassed e = new ExBookingHasPassed();
-			assertEquals("Your booking has passed. Booking cannot be cancelled.",e.getMessage());
-		}
+	    //Facility Test Stub
+	    @Test
+	    public void test_Facility1() {
+	        Facility_Badminton fb = new Facility_Badminton("E1B3");
+	        assertEquals(59, fb.getPrice());
+	    }
+
+	    @Test
+	    public void test_Facility2() {
+	        Facility_Badminton fb = new Facility_Badminton("E1B3");
+	        assertEquals("badminton court", fb.getFacilityType());
+	    }
+
+	    @Test
+	    public void test_Facility3() {
+	        Facility_ActivityRoom ar = new Facility_ActivityRoom("E1A3");
+	        assertEquals(99, ar.getPrice());
+	    }
+
+	    @Test
+	    public void test_Facility4() {
+	        Facility_ActivityRoom ar = new Facility_ActivityRoom("E1A3");
+	        assertEquals("activity room", ar.getFacilityType());
+	    }
+
+	    @Test
+	    public void test_Facility5() {
+	        Facility_TableTennis ar = new Facility_TableTennis("E1T3");
+	        assertEquals(29, ar.getPrice());
+	    }
+
+	    @Test
+	    public void test_Facility6() {
+	        Facility_TableTennis fb = new Facility_TableTennis("E1T3");
+	        assertEquals("table tennis court", fb.getFacilityType());
+	    }
+
+	    @Test
+	    public void test_facility_getFacilityId() {
+	    	Facility testFacility = new Facility_Badminton("A1B1");
+	        assertEquals("A1B1", testFacility.getFacilityId());
+	    }
+
+	    @Test
+	    public void test_facility_getBookingIDByTIme() {
+	        Facility testFacility = new Facility_Badminton("A1B1");
+	        testFacility.addToTimeTable(1112, "20180101A1B11112");
+	        assertEquals("20180101A1B11112", testFacility.getBookingIdbyTime(1112));
+	    }
+
+	    @Test
+	    public void test_facility_getkeyset() {
+	    	Facility testFacility = new Facility_Badminton("A1B1");
+	    	testFacility.addToTimeTable(1112, "20180101A1B11112");
+	    	testFacility.addToTimeTable(1213, "20180101A1B11213");
+	    	Set<Integer> result = testFacility.getkeyset();
+	    	Set<Integer> expectedResult = new HashSet<Integer>();
+	    	expectedResult.add(1112);
+	    	expectedResult.add(1213);
+	    	assertEquals(expectedResult,result);
+	    }
+
+	    @Test
+	    public void test_facility_getTimeTableMap() {
+	    	Facility testFacility = new Facility_Badminton("A1B1");
+	    	testFacility.addToTimeTable(1112, "20180101A1B11112");
+	    	testFacility.addToTimeTable(1213, "20180101A1B11213");
+	    	Map<Integer,String> result = testFacility.getTimetableMap();
+	    	Map<Integer,String> expectedResult = new HashMap<Integer,String>();
+	    	expectedResult.put(1112,"20180101A1B11112");
+	    	expectedResult.put(1213,"20180101A1B11213");
+	    	assertEquals(expectedResult,result);
+	    }
+
+	    @Test
+	    public void test_facility_removeFromTimeTable() {
+	    	Facility testFacility = new Facility_Badminton("A1B1");
+	    	testFacility.addToTimeTable(1112, "20180101A1B11112");
+	    	String result = testFacility.removeFromTimeTable(1112);
+	    	assertEquals("20180101A1B11112",result);
+	    }
+
+	    @Test
+	    public void test_facility_canBook() {
+	    	try {
+				UtilTime mockUtilTime = Whitebox.invokeConstructor(UtilTime.class);
+				PowerMockito.mockStatic(UtilTime.class);
+				Mockito.when(UtilTime.getTimeInstance()).thenReturn(mockUtilTime);
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+
+	    //Exception
+	    @Test
+	    public void test_Exception1() {
+	        ExAllowToBookOneHourOnly e = new ExAllowToBookOneHourOnly();
+	        assertEquals("You are allowed to book 1 hour only for each booking. Please input again.", e.getMessage());
+	    }
+
+	    @Test
+	    public void test_Exception2() {
+	        ExAllowToDeleteOneHourOnly e = new ExAllowToDeleteOneHourOnly();
+	        assertEquals("You are allowed to book 1 hour only for each booking. Please input again.", e.getMessage());
+	    }
+
+	    @Test
+	    public void test_Exception3() {
+	        ExBookingHasPassed e = new ExBookingHasPassed();
+	        assertEquals("Your booking has passed. Booking cannot be cancelled.", e.getMessage());
+	    }
+
+		
 		@Test
 		public void test_Exception4() {
 			ExBookingNotExist e = new ExBookingNotExist();
@@ -370,182 +451,7 @@ public class Test_FSFBS {
 			UtilTime utilTime = new UtilTime();
 			assertEquals(2324,utilTime.getTimeSlot("23:00","00:00"));
 		}
-		//*******************PLEASE DO NOT DELETE BELOW CODE AND ADD TEST CASE UNDER IT*******************//
-		PrintStream oldPrintStream;
-		ByteArrayOutputStream bos;
-
-		private void setOutput()  {
-			oldPrintStream = System.out;
-			bos = new ByteArrayOutputStream();
-			System.setOut(new PrintStream(bos));
-		}
-
-    //S1B vacancy
-    @Test
-    public void test_SportFacilities5() throws ExFacilityIdNotExist, ExFacilityNameNotExist, ExSportCentreNotExist, ExTimeRangeNotCurrent, ExAllowToBookOneHourOnly, ExTimeSlotNotInOpeningHour, ExInputTimeEarlierThanCurrentTime, IOException, ExMemberShipFilePathNotExist, ExSCFilesNotExist, ExIOErrorinGetConfig {
-        UtilTime utilTime = UtilTime.getTimeInstance();
-        Controller controller = Controller.getInstance();
-        SportCentre temp = controller.searchSportCentre("E1");
-        ArrayList<String> list = temp.searchVacanciesInSC(1718, "badminton", utilTime);
-        for (String txt : list)
-            System.out.println(txt);
-    }
-
-    @Test
-    public void test_searchVacancy() {
-        setOutput();
-        User tester = new User("Mr C", "password", "Membership_Adult", "E1", "Facility_Badminton");
-        tester.searchVacancies("E1", "badminton");
-        String output = "\n" + "Sport Centre: Java Road Sports Centre\n" +
-                "\n" +
-                "Facility Code: E1B1\n" +
-                "1011 : Available\n" +
-                "1112 : Available\n" +
-                "1213 : Available\n" +
-                "1314 : Available\n" +
-                "1415 : Available\n" +
-                "1516 : Available\n" +
-                "1617 : Available\n" +
-                "1718 : Available\n" +
-                "1819 : Available\n" +
-                "1920 : Available\n" +
-                "2021 : Available\n" +
-                "2122 : Available\n" +
-                "2223 : Available\n" +
-                "2324 : Available\n" +
-                "\n" +
-                "Facility Code: E1B2\n" +
-                "1011 : Available\n" +
-                "1112 : Available\n" +
-                "1213 : Available\n" +
-                "1314 : Available\n" +
-                "1415 : Available\n" +
-                "1516 : Available\n" +
-                "1617 : Available\n" +
-                "1718 : Available\n" +
-                "1819 : Available\n" +
-                "1920 : Available\n" +
-                "2021 : Available\n" +
-                "2122 : Available\n" +
-                "2223 : Available\n" +
-                "2324 : Available\n\n";
-        assertEquals(output, getOutput());
-    }
-
-    //Facility Test Stub
-    @Test
-    public void test_Facility1() {
-        Facility_Badminton fb = new Facility_Badminton("E1B3");
-        assertEquals(59, fb.getPrice());
-    }
-
-    @Test
-    public void test_Facility2() {
-        Facility_Badminton fb = new Facility_Badminton("E1B3");
-        assertEquals("badminton court", fb.getFacilityType());
-    }
-
-    @Test
-    public void test_Facility3() {
-        Facility_ActivityRoom ar = new Facility_ActivityRoom("E1A3");
-        assertEquals(99, ar.getPrice());
-    }
-
-    @Test
-    public void test_Facility4() {
-        Facility_ActivityRoom ar = new Facility_ActivityRoom("E1A3");
-        assertEquals("activity room", ar.getFacilityType());
-    }
-
-    @Test
-    public void test_Facility5() {
-        Facility_TableTennis ar = new Facility_TableTennis("E1T3");
-        assertEquals(29, ar.getPrice());
-    }
-
-    @Test
-    public void test_Facility6() {
-        Facility_TableTennis fb = new Facility_TableTennis("E1T3");
-        assertEquals("table tennis court", fb.getFacilityType());
-    }
-
-    @Test
-    public void test_facility_getFacilityId() {
-    	Facility testFacility = new Facility_Badminton("A1B1");
-        assertEquals("A1B1", testFacility.getFacilityId());
-    }
-
-    @Test
-    public void test_facility_getBookingIDByTIme() {
-        Facility testFacility = new Facility_Badminton("A1B1");
-        testFacility.addToTimeTable(1112, "20180101A1B11112");
-        assertEquals("20180101A1B11112", testFacility.getBookingIdbyTime(1112));
-    }
-
-    @Test
-    public void test_facility_getkeyset() {
-    	Facility testFacility = new Facility_Badminton("A1B1");
-    	testFacility.addToTimeTable(1112, "20180101A1B11112");
-    	testFacility.addToTimeTable(1213, "20180101A1B11213");
-    	Set<Integer> result = testFacility.getkeyset();
-    	Set<Integer> expectedResult = new HashSet<Integer>();
-    	expectedResult.add(1112);
-    	expectedResult.add(1213);
-    	assertEquals(expectedResult,result);
-    }
-
-    @Test
-    public void test_facility_getTimeTableMap() {
-    	Facility testFacility = new Facility_Badminton("A1B1");
-    	testFacility.addToTimeTable(1112, "20180101A1B11112");
-    	testFacility.addToTimeTable(1213, "20180101A1B11213");
-    	Map<Integer,String> result = testFacility.getTimetableMap();
-    	Map<Integer,String> expectedResult = new HashMap<Integer,String>();
-    	expectedResult.put(1112,"20180101A1B11112");
-    	expectedResult.put(1213,"20180101A1B11213");
-    	assertEquals(expectedResult,result);
-    }
-
-    @Test
-    public void test_facility_removeFromTimeTable() {
-    	Facility testFacility = new Facility_Badminton("A1B1");
-    	testFacility.addToTimeTable(1112, "20180101A1B11112");
-    	String result = testFacility.removeFromTimeTable(1112);
-    	assertEquals("20180101A1B11112",result);
-    }
-
-    @Test
-    public void test_facility_canBook() {
-    	try {
-			UtilTime mockUtilTime = Whitebox.invokeConstructor(UtilTime.class);
-			PowerMockito.mockStatic(UtilTime.class);
-			Mockito.when(UtilTime.getTimeInstance()).thenReturn(mockUtilTime);
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-    }
-
-    //Exception
-    @Test
-    public void test_Exception1() {
-        ExAllowToBookOneHourOnly e = new ExAllowToBookOneHourOnly();
-        assertEquals("You are allowed to book 1 hour only for each booking. Please input again.", e.getMessage());
-    }
-
-    @Test
-    public void test_Exception2() {
-        ExAllowToDeleteOneHourOnly e = new ExAllowToDeleteOneHourOnly();
-        assertEquals("You are allowed to book 1 hour only for each booking. Please input again.", e.getMessage());
-    }
-
-    @Test
-    public void test_Exception3() {
-        ExBookingHasPassed e = new ExBookingHasPassed();
-        assertEquals("Your booking has passed. Booking cannot be cancelled.", e.getMessage());
-    }
+	
 
 
 
